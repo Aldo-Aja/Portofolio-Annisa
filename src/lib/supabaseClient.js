fec7de0e507b101a1d6a8f69c156c014632239d6
Aldo <109517764+Aldo-Aja@users.noreply.github.com>
2026-02-03 12:29:50 +0700

    Update Environtments Variable

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://oovyulbagsbmzeppihxa.supabase.co";
-const supabaseAnonKey = "sb_publishable_HuTJBV8ze_NZgfbUwoLeeg_2wvqo3Ii";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
